ddd7f60bd574c5e7520670e976282e4c52721469
Anuar Mukhambetzhanov <anuar.mukhambetzhanov@gmail.com>
2020-04-17 15:03:38 +0300

    work on search bar and db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,80 @@ paris_texas = Store.create!(name: "Paris Texas", address: "Bereketzade, İstanbu
 file = URI.open('https://res.cloudinary.com/dbjtqhjxi/image/upload/v1586955844/images/ParisTexas/ParisTexasOutside_efzzzd.png')
 paris_texas.photo.attach(io: file, filename: 'paris_texas.png', content_type: 'image/png')
 
-categories = %w(watches jewelleries sunglasses food vintage accessories )
+#New stores
+tuncer_gift_shop = Store.create!(name: "Tuncer Gift Shop", address: "Taya Hatun Sokak Sukran Han NO:C3
+  Sirkeci, Istanbul 34110 Turkey", description:"Tuncer Gift Shop is located in Sultanahmet since 2000s.
+  At the beginning, we started working just on the street and just with one person who is our founder.
+  After a while we expanded our job and we opened first showroom in 2008. When we came to 2012, we have
+  five showrooms in Istanbul and nearly 15 team members.We believe that customers are special; so we
+  made each showroom special by itself. We have two showrooms for ceramics, one shop for special home
+  textile and carpets and one showroom is just for selling jewellery. Our products are also special only
+  for our showrooms and you cannot find the same product in any other shops")
+  file = URI.open('https://media-cdn.tripadvisor.com/media/photo-s/07/a3/a7/ab/tuncer-gift-shop.jpg')
+  tuncer_gift_shop.photo.attach(io: file, filename: 'tuncer_gift_shop.png', content_type: 'image/png')
+
+
+aslan_gift_shop = Store.create!(name: "Aslan Gift Shop", address: "Ankara Cad. Hocapasa Sk. Demir Han
+  No:1 Sirkeci/Istanbul Sirkeci, Istanbul 34080 Turkey", description:"We Are Selling High Quality Souvenirs For Our Visitors
+  To Remind Good Memories Of Istanbul Or Giving Their Friends As A Gift. Nargiles, Nazars, Mugs, Bracel
+  ets And Every Kind Of Turkish Themed Gifts Are Being Sold In Our Fancy Shop In Sirkeci/istanbul.")
+  file = URI.open('https://q-cf.bstatic.com/images/hotel/max1024x768/186/186682383.jpg')
+  aslan_gift_shop.photo.attach(io: file, filename: 'aslan_gift_shop.png', content_type: 'image/png')
+
+tree_of_life_ceramics_and_gift_shop = Store.create!(name: "Tree of Life Ceramics & Gift Shop",
+  address: "Binbirdirek Meydani Sokak no 3/A, Istanbul 34122 Turkey", description:"Tree of Life
+  Ceramics & Gift Shop is newly open and provides new interesting products with an excellent customer
+  service")
+  file = URI.open('https://media-cdn.tripadvisor.com/media/photo-m/1280/1a/da/db/ff/tree-of-life-ceramics.jpg')
+  tree_of_life_ceramics_and_gift_shop.photo.attach(io: file, filename: 'tree_of_life_ceramics_and_gift
+  _shop.png', content_type: 'image/png')
+
+ruby_ceramics_and_gift_shop = Store.create!(name: "Ruby Ceramics & Gift Shop", address: "Boyaci Ahmet
+  Sok. No:22/A, Istanbul Turkey", description:"One of the best ceramics stores in Istanbulheritage_nomadic_art_gallery")
+  file = URI.open('https://media-cdn.tripadvisor.com/media/photo-m/1280/1a/ee/d8/25/ruby-ceramics-gift-shop.jpg')
+  ruby_ceramics_and_gift_shop.photo.attach(io: file, filename: 'ruby_ceramics_and_gift_shop.png', content_type: 'image/png')
+
+heritage_nomadic_art_gallery = Store.create!(name: "Heritage Nomadic Art Gallery", address: "Caferiye Sok. No : 1 34400 Sultan
+  ahmet, Istanbul 34400 Turkey", description:"Discover the magic world of kilims and carpets. Awide range of exceptional villa
+  ge rugs, individually handmade with natural dyes and the finest handspun wool.")
+  file = URI.open('https://media-cdn.tripadvisor.com/media/photo-s/05/0e/84/c4/heritage-nomadic-art.jpg')
+  heritage_nomadic_art_gallery.photo.attach(io: file, filename: 'heritage_nomadic_art_gallery.png', content_type: 'image/png')
+
+rosemary_spice_and_gift = Store.create!(name: "Rosemary spice & gift", address: "kaleci Sokak no 8 Kucukayasofya mah., Istanbul
+ 34122 Turkey", description:"Dear customer our shop near by Sultanahmet mosque we have ceramics special plates and may gift almo
+ st you can find everything about gift in our shop")
+  file = URI.open('https://media-cdn.tripadvisor.com/media/photo-m/1280/1b/00/ea/af/rosemary-spice-gift.jpg')
+  rosemary_spice_and_gift.photo.attach(io: file, filename: 'rosemary_spice_and_gift.png', content_type: 'image/png')
+
+galeria_rugs = Store.create!(name: "Galeria Rugs", address: "Taya Hatun Sk. No. 8E Sirkeci, Istanbul 34122 Turkey",
+  description:"Galeria Rugs was established in 2008. Its founder, Selcuk Orman, has extensive experience in carpets from all over
+  the world having being a director for international carpet galleries and auction houses. Our purpose is to share the true exper
+  ience and knowledge we have of hand woven carpets with enthusiasts and collectors around the world. Since the knowledge of this
+  art form comes with education, we aim to share our knowledge about the intricate technical qualities and human stories behind
+  this unique art.")
+  file = URI.open('https://cdn.fodors.com/ee/files/slideshows/1-What-To-Buy-Istanbul-Rugs.jpg')
+  galeria_rugs.photo.attach(io: file, filename: 'galeria_rugs.png', content_type: 'image/png')
+
+sahaflar_carsisi = Store.create!(name: "Sahaflar Carsisi", address: "Istanbul 34430 Turkey", description:"This historic book bazaar
+ was popular with famous writers of the 16th and 17th centuries. Today, it is one of the most known areas in the city to find rare
+ and used books.")
+  file = URI.open('https://media-cdn.tripadvisor.com/media/photo-s/0d/c4/03/56/sahaflar-carsisi.jpg')
+  sahaflar_carsisi.photo.attach(io: file, filename: 'sahaflar_carsisi.png', content_type: 'image/png')
+
+last_shop_gift_shop = Store.create!(name: "Last Shop Gift Shop", address: "Istanbul, Karakoy, Turkey", description:"Cheeper than Grand Bazaar")
+  file = URI.open('https://media-cdn.tripadvisor.com/media/photo-m/1280/1a/66/4d/a1/last-shop-gift-shop.jpg')
+  last_shop_gift_shop.photo.attach(io: file, filename: 'last_shop_gift_shop.png', content_type: 'image/png')
+
+troy_rug_bazaar = Store.create!(name: "Troy Rug Store", address: "Arasta Bazaar no. 39 Sultanahmet, Istanbul Turkey", description:"Troy Rug Sto
+  re is a long established seller of high quality modern and antique carpets, rugs, kilims and other nomadic art situated in the delightful Ara
+  sta Bazaar in historic Istanbul. We stock pieces from all over Turkey, Iran and Afghanistan. We pride ourselves on a professional and hassle-
+  free selling experience ensuring the customer is delighted with his or her purchase. There is never any obligation to buy and we welcome cust
+  omers who want to browse.Fluent English, Turkish and German spoken.")
+  file = URI.open('https://media-cdn.tripadvisor.com/media/photo-s/05/ab/8d/b8/troy-rug.jpg')
+  troy_rug_bazaar.photo.attach(io: file, filename: 'troy_rug_bazaar.png', content_type: 'image/png')
+
+
+categories = %w(watches jewelleries sunglasses food vintage accessories clothing arts crafts beauty tools sports&outdoors home&kitchen luggage giftcards  )
 categories.each do |category|
   Category.create!(name:category)
 end
@@ -31,12 +104,76 @@ end
 
 
 
-mosaic_table_lamp = Product.create!(name: "Mosaic Table Lamp", description: "Mosaic table lamp is excellent item to have it home.", store: le_wagon_istanbul)
-turkish_tea_pot_model_no1 = Product.create!(name: "Turkish Teapot Stainless Steel Model No1", description: "Turkish Tea Pots (Another name is Çaydanlık) are really important to brew perfect tea.", store: le_wagon_istanbul)
-turkish_tea_pot_model_no2 = Product.create!(name: "Turkish Teapot Stainless Steel Model No2", description: "Turkish Tea Pots (Another name is Çaydanlık) are really important to brew perfect tea.", store: le_wagon_istanbul)
-turkish_tea_pot_model_no3 = Product.create!(name: "Turkish Teapot Stainless Steel Model No3", description: "Turkish Tea Pots (Another name is Çaydanlık) are really important to brew perfect tea.", store: le_wagon_istanbul)
-turkish_tea_pot_model_no4 = Product.create!(name: "Turkish Teapot Stainless Steel Model No4", description: "Turkish Tea Pots (Another name is Çaydanlık) are really important to brew perfect tea.", store: paris_texas)
-prayer_beads = Product.create!(name: "Andiz Tree Prayer Beads", description: "A bead is a small, decorative object that is formed in a variety of shapes and sizes of a material such as stone, bone, shell, glass, plastic, wood or pearl and with a small hole for threading or stringing.", store: galata_grand_bazaar)
+mosaic_table_lamp_1 = Product.create!(name: "Mosaic Table Lamp Model 1", description: "Mosaic table lamp is excellent item to have it home.", store: aslan_gift_shop)
+mosaic_table_lamp_2 = Product.create!(name: "Mosaic Table Lamp Model 2", description: "Mosaic table lamp is excellent item to have it home.", store: aslan_gift_shop)
+mosaic_table_lamp_3 = Product.create!(name: "Mosaic Table Lamp Model 3", description: "Mosaic table lamp is excellent item to have it home.", store: aslan_gift_shop)
+mosaic_table_lamp_4 = Product.create!(name: "Mosaic Table Lamp Model 4", description: "Mosaic table lamp is excellent item to have it home.", store: aslan_gift_shop)
+mosaic_table_lamp_5 = Product.create!(name: "Mosaic Table Lamp Model 5", description: "Mosaic table lamp is excellent item to have it home.", store: aslan_gift_shop)
+mosaic_table_lamp_6 = Product.create!(name: "Mosaic Table Lamp Model 6", description: "Mosaic table lamp is excellent item to have it home.", store: aslan_gift_shop)
+prayer_beads_1 = Product.create!(name: "Andiz Tree Prayer Beads Model 1", description: "A bead is a small, decorative object that is formed in a variety of shapes and
+ sizes of a material such as stone, bone, shell, glass, plastic, wood or pearl and with a small hole for threading or stringing.", store: heritage_nomadic_art_gallery)
+prayer_beads_2 = Product.create!(name: "Andiz Tree Prayer Beads Model 2", description: "A bead is a small, decorative object that is formed in a variety of shapes and
+ sizes of a material such as stone, bone, shell, glass, plastic, wood or pearl and with a small hole for threading or stringing.", store: heritage_nomadic_art_gallery)
+prayer_beads_3 = Product.create!(name: "Andiz Tree Prayer Beads Model 3", description: "A bead is a small, decorative object that is formed in a variety of shapes and
+ sizes of a material such as stone, bone, shell, glass, plastic, wood or pearl and with a small hole for threading or stringing.", store: heritage_nomadic_art_gallery)
+prayer_beads_4 = Product.create!(name: "Andiz Tree Prayer Beads Model 4", description: "A bead is a small, decorative object that is formed in a variety of shapes and
+ sizes of a material such as stone, bone, shell, glass, plastic, wood or pearl and with a small hole for threading or stringing.", store: heritage_nomadic_art_gallery)
+prayer_beads_5 = Product.create!(name: "Andiz Tree Prayer Beads Model 5", description: "A bead is a small, decorative object that is formed in a variety of shapes and
+ sizes of a material such as stone, bone, shell, glass, plastic, wood or pearl and with a small hole for threading or stringing.", store: heritage_nomadic_art_gallery)
+prayer_beads_6 = Product.create!(name: "Andiz Tree Prayer Beads Model 6", description: "A bead is a small, decorative object that is formed in a variety of shapes and
+ sizes of a material such as stone, bone, shell, glass, plastic, wood or pearl and with a small hole for threading or stringing.", store: heritage_nomadic_art_gallery)
+prayer_beads_7 = Product.create!(name: "Andiz Tree Prayer Beads Model 7", description: "A bead is a small, decorative object that is formed in a variety of shapes and
+ sizes of a material such as stone, bone, shell, glass, plastic, wood or pearl and with a small hole for threading or stringing.", store: heritage_nomadic_art_gallery)
+turkish_tea_pot_model_no1 = Product.create!(name: "Turkish Teapot Model No1", description: "Turkish Tea Pots (Another name is Çaydanlık) are
+  really important to brew perfect tea.", store: tuncer_gift_shop)
+turkish_tea_pot_model_no2 = Product.create!(name: "Turkish Teapot Model No2", description: "Turkish Tea Pots (Another name is Çaydanlık) are
+  really important to brew perfect tea.", store: last_shop_gift_shop)
+turkish_tea_pot_model_no3 = Product.create!(name: "Turkish Teapot Model No3", description: "Turkish Tea Pots (Another name is Çaydanlık) are
+  really important to brew perfect tea.", store: sahaflar_carsisi)
+turkish_tea_pot_model_no4 = Product.create!(name: "Turkish Teapot Model No4", description: "Turkish Tea Pots (Another name is Çaydanlık) are
+  really important to brew perfect tea.", store:rosemary_spice_and_gift)
+ray_ban_sunglass_1 = Product.create!(name: "Ray-Ban RB2140 Model 1", description: "The Original Ray-Ban Wayfarer features legendary G-15
+  lenses, in both polarized and non-polarized options, perfect for both women and men. Made from high-quality glass, Ray-Ban lenses are prescription ready.", store:
+  last_shop_gift_shop)
+ray_ban_sunglass_2 = Product.create!(name: "Ray-Ban RB2140 Model 2", description: "The Original Ray-Ban Wayfarer features legendary G-15
+  lenses, in both polarized and non-polarized options, perfect for both women and men. Made from high-quality glass, Ray-Ban lenses are prescription ready.", store:
+  last_shop_gift_shop)
+ray_ban_sunglass_3 = Product.create!(name: "Ray-Ban RB2140 Model 3", description: "The Original Ray-Ban Wayfarer features legendary G-15
+  lenses, in both polarized and non-polarized options, perfect for both women and men. Made from high-quality glass, Ray-Ban lenses are prescription ready.", store:
+  last_shop_gift_shop)
+ray_ban_sunglass_4 = Product.create!(name: "Ray-Ban RB2140 Model 4", description: "The Original Ray-Ban Wayfarer features legendary G-15
+  lenses, in both polarized and non-polarized options, perfect for both women and men. Made from high-quality glass, Ray-Ban lenses are prescription ready.", store:
+  last_shop_gift_shop)
+ray_ban_sunglass_5 = Product.create!(name: "Ray-Ban RB2140 Model 5", description: "The Original Ray-Ban Wayfarer features legendary G-15
+  lenses, in both polarized and non-polarized options, perfect for both women and men. Made from high-quality glass, Ray-Ban lenses are prescription ready.", store:
+  last_shop_gift_shop)
+ray_ban_sunglass_6 = Product.create!(name: "Ray-Ban RB2140 Model 6", description: "The Original Ray-Ban Wayfarer features legendary G-15
+  lenses, in both polarized and non-polarized options, perfect for both women and men. Made from high-quality glass, Ray-Ban lenses are prescription ready.", store:
+  last_shop_gift_shop)
+ray_ban_sunglass_7 = Product.create!(name: "Ray-Ban RB2140 Model 7", description: "The Original Ray-Ban Wayfarer features legendary G-15
+  lenses, in both polarized and non-polarized options, perfect for both women and men. Made from high-quality glass, Ray-Ban lenses are prescription ready.", store:
+  last_shop_gift_shop)
+ray_ban_sunglass_8 = Product.create!(name: "Ray-Ban RB2140 Model 8", description: "The Original Ray-Ban Wayfarer features legendary G-15
+  lenses, in both polarized and non-polarized options, perfect for both women and men. Made from high-quality glass, Ray-Ban lenses are prescription ready.", store:
+  last_shop_gift_shop)
+ray_ban_sunglass_9 = Product.create!(name: "Ray-Ban RB2140 Model 9", description: "The Original Ray-Ban Wayfarer features legendary G-15
+  lenses, in both polarized and non-polarized options, perfect for both women and men. Made from high-quality glass, Ray-Ban lenses are prescription ready.", store:
+  last_shop_gift_shop)
+ray_ban_sunglass_10 = Product.create!(name: "Ray-Ban RB2140 Model 10", description: "The Original Ray-Ban Wayfarer features legendary G-15
+  lenses, in both polarized and non-polarized options, perfect for both women and men. Made from high-quality glass, Ray-Ban lenses are prescription ready.", store:
+  last_shop_gift_shop)
+
+
+
+
+
+
+
+
+
+
+
+
 puts "seeds are created"
 
 
